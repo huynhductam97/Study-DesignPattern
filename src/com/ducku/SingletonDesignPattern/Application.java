b305ec2a5fea37f179c6fdbb241c77f342b95c6a
email <huynhductam69@gmail.com>
2022-08-12 17:59:56 +0700

    ku hoc singleton pattern

---
+++ src/com/ducku/SingletonDesignPattern/Application.java
@@ -0,0 +1,13 @@
+package com.ducku.SingletonDesignPattern;
+
+public class Application {
+
+  public static void main(String[] args) {
+    Thread t1 = new Thread(() -> DuckuSingleton.getInstance().myNumber());
+    Thread t2 = new Thread(() -> DuckuSingleton.getInstance().myNumber());
+
+    t1.start();
+    t2.start();
+
+  }
+}
---
+++ src/com/ducku/SingletonDesignPattern/DuckuSingleton.java
@@ -0,0 +1,27 @@
+package com.ducku.SingletonDesignPattern;
+
+import java.util.Random;
+
+public class DuckuSingleton {
+
+  private int index;
+  private volatile static DuckuSingleton ducku;
+
+  private DuckuSingleton(int index) {
+    this.index = index;
+  }
+
+  public static DuckuSingleton getInstance() {
+    synchronized (DuckuSingleton.class) {
+      if (ducku == null) {
+        Random random = new Random();
+        ducku = new DuckuSingleton(random.nextInt(5) + 1);
+      }
+    }
+    return ducku;
+  }
+
+  public void myNumber() {
+    System.out.println("My number is: " + ducku.index);
+  }
+}
